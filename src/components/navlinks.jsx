1a470403d8fd5a0099dea2d34a95b5dc340a21ae
Yash-Mobikasa <150777724+Yash-Mobikasa@users.noreply.github.com>
2024-06-26 00:16:23 +0530

    Added Hover effect for the nevlinks

--- src/components/navlinks.jsx
+++ src/components/navlinks.jsx
@@ -1,12 +1,21 @@
 "use client"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import { Inter,Roboto,Roboto_Mono } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
 const NavLinks = ({link,inx}) => {
     const pathName = usePathname()
   return (
-    <Link href={link.url} key={inx} className={`rounded ${pathName === link.url && "bg-black text-white"}`}>{link.title}</Link>
+    <Link href={link.url} key={inx} className={` ${pathName === link.url && `relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-100 ${roboto_mono.className} font-sans font-bold`} relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center `}>{link.title}</Link>
 )
 }
 
